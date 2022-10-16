13f475147a4f5be25ddfa157e0647991af1e9895
Facundo Montiel <montiel.facu@gmail.com>
2022-10-16 20:53:51 -0300

    Trabaja practico numero 2

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Saludo from "./Saludo"
+
+function App() {
+
+  return (
+    <Saludo saludo="my friend"></Saludo>
+  )
+}
+
+export default App
---
+++ src/Saludo.jsx
@@ -0,0 +1,6 @@
+const Saludo = ({saludo})=>{
+    return(
+        <h1>Hello {saludo}</h1>
+    );
+}
+export default Saludo;
